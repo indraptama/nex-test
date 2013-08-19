4bf4fa71de88d48d03949be23bfb07f540150c93
indraptama <indra.symfonia@gmail.com>
2013-08-20 00:39:25 +0700

    add new java script

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 $('.toggle').click(function(){
 	$('.main-nav').toggleClass('show');
 
+});
+
+$(document).ready(function() {
+  $("img").unveil();
+});
 
 
-})
+// pure JS
+var elem = document.getElementById('mySwipe');
+window.mySwipe = Swipe(elem, {
+  // startSlide: 4,
+   auto: 5000,
+  // continuous: true,
+  // disableScroll: true,
+  // stopPropagation: true,
+  // callback: function(index, element) {},
+  // transitionEnd: function(index, element) {}
+});
+
+$('.bxslider').bxSlider({
+  minSlides: 1,
+  maxSlides: 2,
+  slideWidth: 360,
+  slideMargin: 10
+});
